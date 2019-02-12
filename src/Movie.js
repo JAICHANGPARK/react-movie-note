0d5ab42668451c300da77d6892bd95229c30de94
Dreamwalker <aristojeff@gmail.com>
2019-02-12 09:51:55 +0900

    Create Movie Poster

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,25 @@
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Movie.css';
 
-class Movie extends Component{
-    render(){
-        return(
+class Movie extends Component {
+    render() {
+        return (
             <div>
-                
+                <MoviePoster></MoviePoster>
+                <h1>Hello this is a moview </h1>
             </div>
         )
     }
 }
+
+
+class MoviePoster extends Component{
+    render(){
+        return(
+            <img src="https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"/>
+        )
+    }
+}
+
+export default Movie
