a4ad08c686f00d846e22a36e45bd7ae6bbb29158
Dreamwalker <aristojeff@gmail.com>
2019-02-18 08:54:44 +0900

    Update State

--- src/App.js
+++ src/App.js
@@ -10,34 +10,57 @@ const movieTitles = [
   "Star Wars"
 ];
 
-const movieImage =[];
+const movieImage = [];
 
-const movies =[
+const movies = [
   {
-    
-    title :"Matrix",
-    poster : "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
+
+    title: "Matrix",
+    poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
   },
   {
-    title :"Matrix",
-    poster : "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
+    title: "Matrix",
+    poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
   },
   {
-    title :"Matrix",
-    poster : "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
+    title: "Matrix",
+    poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
   },
 
 ];
+// Render  : componentWillMount () => render => compoonentDidMount()
+// Update :  compoenent Will ReceiveProps() -> ShouldComponentUpdate() -> CompoenetWillUpdate -> render -> componentDidUpdate 
+// State : React 컴포넌트 안에 있는 오브젝트, 컴포넌트 안에 State가 바뀔때 마다, 다시 Render 수행 
 
 class App extends Component {
+
+  state = {
+    greeting: "Hello"
+  }
+
+  componentWillMount() {
+    //api에 작업 요청
+    console.log("componentWillMuount")
+  }
+  componentDidMount() {
+    //render후 작업 처리 
+    setTimeout(() => {
+      //State는 직접 변경할 수 없고 setState를 통해 데이터 변경 처리 가능 .
+      this.setState({
+        greeting : "someting"
+      })
+
+    }, 5000)
+  }
+
   render() {
     return (
       <div className="App">
-      {movies.map((movie, index) =>{
+        {this.state.greeting}
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
+        })}
 
-        return <Movie title={movie.title} poster ={movie.poster} key={index}/>
-      })}
-     
       </div>
     );
   }
