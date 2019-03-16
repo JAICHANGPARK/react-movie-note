c0a019d97293410c075dbf08dd15a71d0be4abd7
Dreamwalker <aristojeff@gmail.com>
2019-03-16 23:15:59 +0900

    Update Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,72 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
 import './Movie.css';
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired,
-        genres: PropTypes.string.isRequired,
-        synopsis: PropTypes.string.isRequired,
-    }
-    
-    render() {
-        console.log(this.props);
-        return (
-            <div className="Movie">
-                <div className="Movie__Columns">
-                    <MoviePoster poster={this.props.poster}  alt={this.props.title}/>
+function Movie({title, poster, genres, synopsis}){
+    return (
+        <div className="Movie">
+            <div className="Movie__Column">
+                <MoviePoster poster={poster} alt={title} />
+            </div>   
+            <div className="Movie__Column">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
                 </div>
-                <div className="Movie__Columns">
-                    <h1>{this.props.title} </h1>
-                    <div className="Movie__Genres">
-                        {this.props.genres.map((genre, index) =>
-                            <MovieGenre genres={genre} key={index}/> )}
-                    </div>
-                    <p className="Movie__Synopsis">
-                    {this.props.synopsis}
-                    </p>
-
+                <div className="Movie__Synopsis">
+                {synopsis}
                 </div>
-
-
-                {/* jsx의 경우 명령어를 실행하려면 꼭 괄호를 넣어야함. */}
-
             </div>
-        )
-    }
-}
-
-function MovieGenre({ genres }) {
-    return (
-        <span className="Movie__Genre">{genres} </span>
+        </div>
     )
 }
 
-// class MoviePoster extends Component{
-//     static propTypes ={
-//         poster : PropTypes.string.isRequired
-//     }
-//     render(){
-//         return(
-//             <img src={this.props.poster} alt = "Movie Poster"/>
-//         )
-//     }
-// }
-function MoviePoster({ poster, alt }) {
+function MoviePoster({poster, alt}){
     return (
-        <img src={poster} alt={alt} title ={alt}  className ="Movie__Poster"/>
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
     )
+}
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
     poster: PropTypes.string.isRequired,
     alt: PropTypes.string.isRequired
 }
-MovieGenre.propTypes = {
-    genres: PropTypes.string.isRequired
+
+MovieGenre.propTypes ={
+    genre: PropTypes.string.isRequired
 }
 
 export default Movie
