3feea66967cf71d69c4a65972972523200968f41
Dreamwalker <aristojeff@gmail.com>
2019-03-16 20:02:24 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,22 +12,21 @@ const movieTitles = [
 
 const movieImage = [];
 
-const movies = [
-  {
+// const movies = [
+//   {
 
-    title: "Matrix",
-    poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
-  },
-  {
-    title: "Matrix",
-    poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
-  },
-  {
-    title: "Matrix",
-    poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
-  },
-
-];
+//     title: "Matrix",
+//     poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
+//   },
+//   {
+//     title: "Matrix",
+//     poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
+//   },
+//   {
+//     title: "Matrix",
+//     poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
+//   },
+// ];
 // Render  : componentWillMount () => render => compoonentDidMount()
 // Update :  compoenent Will ReceiveProps() -> ShouldComponentUpdate() -> CompoenetWillUpdate -> render -> componentDidUpdate 
 // State : React 컴포넌트 안에 있는 오브젝트, 컴포넌트 안에 State가 바뀔때 마다, 다시 Render 수행 
@@ -35,20 +34,45 @@ const movies = [
 class App extends Component {
 
   state = {
-    greeting: "Hello"
+    greeting: "Hello",
+    movies: [
+      {
+
+        title: "Matrix",
+        poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
+      },
+      {
+        title: "Matrix",
+        poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
+      },
+      {
+        title: "Matrix",
+        poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
+      },
+    ],
+
   }
 
   componentWillMount() {
     //api에 작업 요청
     console.log("componentWillMuount")
   }
-  
+
   componentDidMount() {
     //render후 작업 처리 
     setTimeout(() => {
+      this.setState({
+        movies: [
+          ...this.state.movies,
+          {
+            title: "Matrix",
+            poster: "https://t1.daumcdn.net/cfile/tistory/99256D3359D0C7D02A"
+          }
+        ]
+      })
       //State는 직접 변경할 수 없고 setState를 통해 데이터 변경 처리 가능 .
       this.setState({
-        greeting : "someting"
+        greeting: "someting"
       })
 
     }, 5000)
@@ -57,8 +81,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {/* {this.state.greeting} */}
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
 
